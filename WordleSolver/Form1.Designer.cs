9d4c3973759e4c3908fa78c7e3f89fc327f42f68
Kev Bourque <kevin.bourque@nbcc.ca>
2022-01-28 09:21:33 -0400

    Initial commit

---
+++ WordleSolver/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace WordleSolver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.btnL1c = new System.Windows.Forms.Button();
+            this.txtL1 = new System.Windows.Forms.TextBox();
+            this.txtL2 = new System.Windows.Forms.TextBox();
+            this.txtL4 = new System.Windows.Forms.TextBox();
+            this.txtL3 = new System.Windows.Forms.TextBox();
+            this.txtL5 = new System.Windows.Forms.TextBox();
+            this.btnL1b = new System.Windows.Forms.Button();
+            this.btnL1w = new System.Windows.Forms.Button();
+            this.lstAnswers = new System.Windows.Forms.ListBox();
+            this.btnL2w = new System.Windows.Forms.Button();
+            this.btnL2b = new System.Windows.Forms.Button();
+            this.btnL2c = new System.Windows.Forms.Button();
+            this.btnL3w = new System.Windows.Forms.Button();
+            this.btnL3b = new System.Windows.Forms.Button();
+            this.btnL3c = new System.Windows.Forms.Button();
+            this.btnL4w = new System.Windows.Forms.Button();
+            this.btnL4b = new System.Windows.Forms.Button();
+            this.btnL4c = new System.Windows.Forms.Button();
+            this.btnL5w = new System.Windows.Forms.Button();
+            this.btnL5b = new System.Windows.Forms.Button();
+            this.btnL5c = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(539, 49);
+            this.btnSolve.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(195, 28);
+            this.btnSolve.TabIndex = 0;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // btnL1c
+            // 
+            this.btnL1c.Location = new System.Drawing.Point(39, 160);
+            this.btnL1c.Name = "btnL1c";
+            this.btnL1c.Size = new System.Drawing.Size(87, 31);
+            this.btnL1c.TabIndex = 6;
+            this.btnL1c.Text = "Correct";
+            this.btnL1c.UseVisualStyleBackColor = true;
+            this.btnL1c.Click += new System.EventHandler(this.btnL1c_Click);
+            // 
+            // txtL1
+            // 
+            this.txtL1.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtL1.Location = new System.Drawing.Point(39, 49);
+            this.txtL1.MaxLength = 1;
+            this.txtL1.Name = "txtL1";
+            this.txtL1.Size = new System.Drawing.Size(87, 105);
+            this.txtL1.TabIndex = 7;
+            this.txtL1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtL2
+            // 
+            this.txtL2.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtL2.Location = new System.Drawing.Point(132, 49);
+            this.txtL2.MaxLength = 1;
+            this.txtL2.Name = "txtL2";
+            this.txtL2.Size = new System.Drawing.Size(87, 105);
+            this.txtL2.TabIndex = 8;
+            this.txtL2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtL4
+            // 
+            this.txtL4.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtL4.Location = new System.Drawing.Point(318, 49);
+            this.txtL4.MaxLength = 1;
+            this.txtL4.Name = "txtL4";
+            this.txtL4.Size = new System.Drawing.Size(87, 105);
+            this.txtL4.TabIndex = 10;
+            this.txtL4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtL3
+            // 
+            this.txtL3.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtL3.Location = new System.Drawing.Point(225, 49);
+            this.txtL3.MaxLength = 1;
+            this.txtL3.Name = "txtL3";
+            this.txtL3.Size = new System.Drawing.Size(87, 105);
+            this.txtL3.TabIndex = 9;
+            this.txtL3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtL5
+            // 
+            this.txtL5.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtL5.Location = new System.Drawing.Point(411, 49);
+            this.txtL5.MaxLength = 1;
+            this.txtL5.Name = "txtL5";
+            this.txtL5.Size = new System.Drawing.Size(87, 105);
+            this.txtL5.TabIndex = 11;
+            this.txtL5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnL1b
+            // 
+            this.btnL1b.Location = new System.Drawing.Point(39, 197);
+            this.btnL1b.Name = "btnL1b";
+            this.btnL1b.Size = new System.Drawing.Size(87, 31);
+            this.btnL1b.TabIndex = 12;
+            this.btnL1b.Text = "Bad Spot";
+            this.btnL1b.UseVisualStyleBackColor = true;
+            this.btnL1b.Click += new System.EventHandler(this.btnL1b_Click);
+            // 
+            // btnL1w
+            // 
+            this.btnL1w.Location = new System.Drawing.Point(39, 234);
+            this.btnL1w.Name = "btnL1w";
+            this.btnL1w.Size = new System.Drawing.Size(87, 31);
+            this.btnL1w.TabIndex = 13;
+            this.btnL1w.Text = "Wrong";
+            this.btnL1w.UseVisualStyleBackColor = true;
+            this.btnL1w.Click += new System.EventHandler(this.btnL1w_Click);
+            // 
+            // lstAnswers
+            // 
+            this.lstAnswers.FormattingEnabled = true;
+            this.lstAnswers.ItemHeight = 16;
+            this.lstAnswers.Location = new System.Drawing.Point(539, 84);
+            this.lstAnswers.Name = "lstAnswers";
+            this.lstAnswers.Size = new System.Drawing.Size(195, 148);
+            this.lstAnswers.TabIndex = 14;
+            // 
+            // btnL2w
+            // 
+            this.btnL2w.Location = new System.Drawing.Point(132, 234);
+            this.btnL2w.Name = "btnL2w";
+            this.btnL2w.Size = new System.Drawing.Size(87, 31);
+            this.btnL2w.TabIndex = 17;
+            this.btnL2w.Text = "Wrong";
+            this.btnL2w.UseVisualStyleBackColor = true;
+            this.btnL2w.Click += new System.EventHandler(this.btnL2w_Click);
+            // 
+            // btnL2b
+            // 
+            this.btnL2b.Location = new System.Drawing.Point(132, 197);
+            this.btnL2b.Name = "btnL2b";
+            this.btnL2b.Size = new System.Drawing.Size(87, 31);
+            this.btnL2b.TabIndex = 16;
+            this.btnL2b.Text = "Bad Spot";
+            this.btnL2b.UseVisualStyleBackColor = true;
+            this.btnL2b.Click += new System.EventHandler(this.btnL2b_Click);
+            // 
+            // btnL2c
+            // 
+            this.btnL2c.Location = new System.Drawing.Point(132, 160);
+            this.btnL2c.Name = "btnL2c";
+            this.btnL2c.Size = new System.Drawing.Size(87, 31);
+            this.btnL2c.TabIndex = 15;
+            this.btnL2c.Text = "Correct";
+            this.btnL2c.UseVisualStyleBackColor = true;
+            this.btnL2c.Click += new System.EventHandler(this.btnL2c_Click);
+            // 
+            // btnL3w
+            // 
+            this.btnL3w.Location = new System.Drawing.Point(225, 234);
+            this.btnL3w.Name = "btnL3w";
+            this.btnL3w.Size = new System.Drawing.Size(87, 31);
+            this.btnL3w.TabIndex = 20;
+            this.btnL3w.Text = "Wrong";
+            this.btnL3w.UseVisualStyleBackColor = true;
+            this.btnL3w.Click += new System.EventHandler(this.btnL3w_Click);
+            // 
+            // btnL3b
+            // 
+            this.btnL3b.Location = new System.Drawing.Point(225, 197);
+            this.btnL3b.Name = "btnL3b";
+            this.btnL3b.Size = new System.Drawing.Size(87, 31);
+            this.btnL3b.TabIndex = 19;
+            this.btnL3b.Text = "Bad Spot";
+            this.btnL3b.UseVisualStyleBackColor = true;
+            this.btnL3b.Click += new System.EventHandler(this.btnL3b_Click);
+            // 
+            // btnL3c
+            // 
+            this.btnL3c.Location = new System.Drawing.Point(225, 160);
+            this.btnL3c.Name = "btnL3c";
+            this.btnL3c.Size = new System.Drawing.Size(87, 31);
+            this.btnL3c.TabIndex = 18;
+            this.btnL3c.Text = "Correct";
+            this.btnL3c.UseVisualStyleBackColor = true;
+            this.btnL3c.Click += new System.EventHandler(this.btnL3c_Click);
+            // 
+            // btnL4w
+            // 
+            this.btnL4w.Location = new System.Drawing.Point(318, 234);
+            this.btnL4w.Name = "btnL4w";
+            this.btnL4w.Size = new System.Drawing.Size(87, 31);
+            this.btnL4w.TabIndex = 23;
+            this.btnL4w.Text = "Wrong";
+            this.btnL4w.UseVisualStyleBackColor = true;
+            this.btnL4w.Click += new System.EventHandler(this.btnL4w_Click);
+            // 
+            // btnL4b
+            // 
+            this.btnL4b.Location = new System.Drawing.Point(318, 197);
+            this.btnL4b.Name = "btnL4b";
+            this.btnL4b.Size = new System.Drawing.Size(87, 31);
+            this.btnL4b.TabIndex = 22;
+            this.btnL4b.Text = "Bad Spot";
+            this.btnL4b.UseVisualStyleBackColor = true;
+            this.btnL4b.Click += new System.EventHandler(this.btnL4b_Click);
+            // 
+            // btnL4c
+            // 
+            this.btnL4c.Location = new System.Drawing.Point(318, 160);
+            this.btnL4c.Name = "btnL4c";
+            this.btnL4c.Size = new System.Drawing.Size(87, 31);
+            this.btnL4c.TabIndex = 21;
+            this.btnL4c.Text = "Correct";
+            this.btnL4c.UseVisualStyleBackColor = true;
+            this.btnL4c.Click += new System.EventHandler(this.btnL4c_Click);
+            // 
+            // btnL5w
+            // 
+            this.btnL5w.Location = new System.Drawing.Point(411, 234);
+            this.btnL5w.Name = "btnL5w";
+            this.btnL5w.Size = new System.Drawing.Size(87, 31);
+            this.btnL5w.TabIndex = 26;
+            this.btnL5w.Text = "Wrong";
+            this.btnL5w.UseVisualStyleBackColor = true;
+            this.btnL5w.Click += new System.EventHandler(this.btnL5w_Click);
+            // 
+            // btnL5b
+            // 
+            this.btnL5b.Location = new System.Drawing.Point(411, 197);
+            this.btnL5b.Name = "btnL5b";
+            this.btnL5b.Size = new System.Drawing.Size(87, 31);
+            this.btnL5b.TabIndex = 25;
+            this.btnL5b.Text = "Bad Spot";
+            this.btnL5b.UseVisualStyleBackColor = true;
+            this.btnL5b.Click += new System.EventHandler(this.btnL5b_Click);
+            // 
+            // btnL5c
+            // 
+            this.btnL5c.Location = new System.Drawing.Point(411, 160);
+            this.btnL5c.Name = "btnL5c";
+            this.btnL5c.Size = new System.Drawing.Size(87, 31);
+            this.btnL5c.TabIndex = 24;
+            this.btnL5c.Text = "Correct";
+            this.btnL5c.UseVisualStyleBackColor = true;
+            this.btnL5c.Click += new System.EventHandler(this.btnL5c_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(539, 239);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(195, 28);
+            this.btnReset.TabIndex = 27;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 324);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnL5w);
+            this.Controls.Add(this.btnL5b);
+            this.Controls.Add(this.btnL5c);
+            this.Controls.Add(this.btnL4w);
+            this.Controls.Add(this.btnL4b);
+            this.Controls.Add(this.btnL4c);
+            this.Controls.Add(this.btnL3w);
+            this.Controls.Add(this.btnL3b);
+            this.Controls.Add(this.btnL3c);
+            this.Controls.Add(this.btnL2w);
+            this.Controls.Add(this.btnL2b);
+            this.Controls.Add(this.btnL2c);
+            this.Controls.Add(this.lstAnswers);
+            this.Controls.Add(this.btnL1w);
+            this.Controls.Add(this.btnL1b);
+            this.Controls.Add(this.txtL5);
+            this.Controls.Add(this.txtL4);
+            this.Controls.Add(this.txtL3);
+            this.Controls.Add(this.txtL2);
+            this.Controls.Add(this.txtL1);
+            this.Controls.Add(this.btnL1c);
+            this.Controls.Add(this.btnSolve);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Wordle Solver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Button btnL1c;
+        private System.Windows.Forms.TextBox txtL1;
+        private System.Windows.Forms.TextBox txtL2;
+        private System.Windows.Forms.TextBox txtL4;
+        private System.Windows.Forms.TextBox txtL3;
+        private System.Windows.Forms.TextBox txtL5;
+        private System.Windows.Forms.Button btnL1b;
+        private System.Windows.Forms.Button btnL1w;
+        private System.Windows.Forms.ListBox lstAnswers;
+        private System.Windows.Forms.Button btnL2w;
+        private System.Windows.Forms.Button btnL2b;
+        private System.Windows.Forms.Button btnL2c;
+        private System.Windows.Forms.Button btnL3w;
+        private System.Windows.Forms.Button btnL3b;
+        private System.Windows.Forms.Button btnL3c;
+        private System.Windows.Forms.Button btnL4w;
+        private System.Windows.Forms.Button btnL4b;
+        private System.Windows.Forms.Button btnL4c;
+        private System.Windows.Forms.Button btnL5w;
+        private System.Windows.Forms.Button btnL5b;
+        private System.Windows.Forms.Button btnL5c;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
